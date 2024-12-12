508dffcfd0e05aa869b529610b8aa0ce99322cfe
kawaiikoala <74r2k@naver.com>
2024-12-12 16:36:27 +0900

    commit
    commit

---
+++ 02_소스코드/김용환/프론트/ITN2024f/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 머리말 : HeaderCom -->
+  <HeaderCom />
+  <!-- 본문 -->
+  <div class="container mt-3 mb-200" >
+    <router-view />
+  </div>
+  <FooterCom />
+
+
+</template>
+<script>
+import HeaderCom from "./components/HeaderCom.vue";
+import FooterCom from "./components/FooterCom.vue";
+export default {
+  components: {
+    HeaderCom,
+    FooterCom,
+  },
+};
+</script>
+<style>
+@import "@/assets/css/main.css";
+</style>
